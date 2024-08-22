96c2d42f68a1deb12144042f36ededcc28a73615
kwon <93umsongaji@gmail.com>
2024-08-23 00:07:43 +0900

    day1

--- js/basic/day1.js
+++ js/basic/day1.js
@@ -6,6 +6,21 @@
  */
 
 //함수를 인자로 받아서 실행하는 함수
+const apply1 = f => f(1);
+const add2 = a => a + 2;
+console.log(apply1(add2));
 
-const apply1 = a => f(1);
+const times = (f, n) => {
+  let i = -1;
+  while (++i < n) f(i);
+};
+
+//함수를 값으로 받아 실행.
+times(console.log, 3);
+
+//함수를 만들어 리턴하는 함수 (클로저를 만들어 리턴하는 함수)
+const addMaker = a => b => a + b;
+const add10 = addMaker(10);
+console.log(add10(5));
+
 
